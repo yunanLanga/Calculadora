ea61974f670dcd8728c1f37c66f17b30c4735807
tubvision <87477436+yunanLanga@users.noreply.github.com>
2022-11-15 21:37:47 -1200

    primeiro commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
-<template>
+ <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <Calculadora/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    import Calculadora from "./main/Calculadora.vue"
+    export default { 
+       components:{Calculadora}
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+     @font-face {
+        font-family: "RobotoMono";
+        src: url("./fonts/RobotoMono-Thin.ttf");
+     }
+
+     *{
+         font-family: "RobotoMono", monospace;
+     }
+
+     body{
+        margin: 0;
+     }
+
+   
+     #app{
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        color: #fff;
+        background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73)) ;
+     }
 </style>
